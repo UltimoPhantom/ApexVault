3f1ea797f72cef4c916d882a6df8c77b59262348
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-01-19 01:28:37 +0530

    Working!!

--- Backend/AWS/server.py
+++ Backend/AWS/server.py
@@ -1,4 +1,23 @@
+import os
 import pymongo
-from dotenv import MONGO_STRING 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_STRING = os.getenv("MONGO_STRING")
 
 client = pymongo.MongoClient(MONGO_STRING)
+db = client["Stocks"]
+
+collection = db["stocks"]
+
+newDoc = {
+    "name": "HDFCBANK",
+    "price": 1550,
+    "quantity": 10,
+    "LTP": 1450
+}
+
+collection.insert_one(newDoc)
+
+
