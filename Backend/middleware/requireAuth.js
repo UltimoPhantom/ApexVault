a59c827f604c82d887705173c8c17bb764ddf91b
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-02-01 23:32:54 +0530

    Route blocking

---
+++ Backend/middleware/requireAuth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import { jwt_sign }from '../config.js'
+import { User } from "../modules/userModels.js";
+
+const requireAuth = async (req, res, next) => {
+
+    const { authorization } = req.headers
+
+    if(!authorization) {
+        return res.status(401).json({ error: 'Auth token required!' })
+    }
+
+    const token = authorization.split(' ')[1]
+
+    try {
+        const {_id} = jwt.verify(token, jwt_sign)
+
+        req.user = await User.findOne({ _id }).select('_id')
+        next()
+    }
+    catch(error) {
+        console.log(error)
+        res.status(401).json({ error: 'Request is not authorized' })
+    }
+}
+
+export default requireAuth
