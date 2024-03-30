93b496740ba539fc56fe2a76ae9498724eed3682
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-03-30 10:24:03 +0530

    Added new button design

--- frontend/src/components/AddStockButton.js
+++ frontend/src/components/AddStockButton.js
@@ -59,9 +59,16 @@ const CustomDialog = () => {
 
     return (
         <div>
-            <button onClick={handleClickOpen} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-16">
+        <a href="#_" className="relative inline-flex items-center justify-start px-8 py-4 overflow-hidden font-medium transition-all bg-green-500 rounded-xl group" onClick={handleClickOpen}>
+            <span className="absolute top-0 right-0 inline-block w-5 h-5 transition-all duration-500 ease-in-out bg-green-700 rounded group-hover:-mr-5 group-hover:-mt-5">
+                <span className="absolute top-0 right-0 w-6 h-6 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+            </span>
+            <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-green-600 rounded-2xl group-hover:mb-16 group-hover:translate-x-0"></span>
+            <span className="relative w-full text-left font-bold text-white transition-colors duration-200 ease-in-out group-hover:text-white">Add Stocks!</span>
+        </a>
+            {/* <button onClick={handleClickOpen} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-16">
                 Add Stocks
-            </button>
+            </button> */}
             {isOpen && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center ">
                     <div className="absolute inset-0 bg-gray-900 opacity-75" onClick={handleClose}></div>
