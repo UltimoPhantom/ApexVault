d52bc0b8991e02ceb17795f262f63d2776a2d8fb
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-03-30 10:30:30 +0530

    Changed logout button

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -9,13 +9,13 @@ const LogoutButton = () => {
     };
 
     return (
-        <button
-            type="button"
-            className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 mr-14"
-            onClick={handleLogoutClick} // Attach onClick event handler
-        >
-            Logout
-        </button>
+        <div className='ml-4'>
+            <a href="#_" className="relative inline-flex items-center justify-center px-8 py-4 overflow-hidden  font-medium tracking-tighter text-white bg-gray-800 rounded-lg group" onClick={handleLogoutClick}>
+                <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-red-700 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+                <span className="relative font-bold">Logout</span>
+            </a>
+        </div>
     );
 };
 
